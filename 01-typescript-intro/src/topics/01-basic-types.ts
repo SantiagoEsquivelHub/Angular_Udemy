82ce592cbbe9fa2165864afb431fca0b6f6597d1
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-08-07 13:10:22 -0500

    basic types and general concepts

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,17 @@
+const name = "Santiago";
+let hpPoints: number | string = 95;
+const isAlive: boolean = true;
+
+hpPoints = "FULL";
+
+console.log({
+    name,
+    hpPoints,
+    isAlive
+})
+
+
+hpPoints = "FULL"
+
+
+export { };
