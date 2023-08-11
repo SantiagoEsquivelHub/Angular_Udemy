8a0e1d86e92bd9c60c46a4e5c8b7a5802c792bf0
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-08-10 23:46:23 -0500

    parameters destructuring

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,42 @@
+interface Product {
+    description: string;
+    price: number;
+}
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+const phone: Product = {
+    description: "Nokia A1",
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: "iPad Air",
+    price: 250.0
+}
+
+
+const taxCalculation = ({ products, tax }: TaxCalculationOptions): number[] => {
+    let total = 0;
+
+    products.map(({ price }) => total += price)
+
+    return [total, total * tax]
+}
+
+
+const shoppingCart: Product[] = [phone, tablet];
+const tax = 0.15;
+
+const [total, newTax] = taxCalculation({
+    tax,
+    products: shoppingCart
+})
+console.log("🚀 ~ file: 06-function-destructuring.ts:38 ~ total:", total)
+console.log("🚀 ~ file: 06-function-destructuring.ts:38 ~ newTax:", newTax)
+
+
+export { };
