9b179d7cea6b2f8eb655f4a94d744aae0a82551e
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-08-21 17:30:45 -0500

    init dbz module

---
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DBZMainPageComponent } from './pages/main-page.component';
+
+@NgModule({
+  declarations: [
+    DBZMainPageComponent
+  ],
+  exports: [
+    DBZMainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+})
+export class DbzModule { }
