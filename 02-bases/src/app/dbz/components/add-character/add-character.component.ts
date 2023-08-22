9f2ccf10c7a93812602c86f8e9fa3e2d0f460aa4
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-08-21 21:16:27 -0500

    interfaces, input(props), ngForn props and ngClass

--- 02-bases/src/app/dbz/components/add-character/add-character.component.ts
+++ 02-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dbz-add-character',
+  selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
   styleUrls: ['./add-character.component.css']
 })
 export class AddCharacterComponent {
+
+
 
 }
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
-export class MainPageComponent implements OnInit {
-  constructor() { }
+export class MainPageComponent {
 
-  ngOnInit() { }
+  public characters: Character[] = [
+    {
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      name: 'Gokú',
+      power: 9500,
+    },
+    {
+      name: 'Trunks',
+      power: 10,
+    },
+  ];
+
 }
