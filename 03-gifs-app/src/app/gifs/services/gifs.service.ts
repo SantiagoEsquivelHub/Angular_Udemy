213e4da9e8c3e7b8534629241137513a00fea7d1
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-09-24 20:50:11 -0500

    interfaces and get gifs

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,9 @@ export class GifsService {
 
   private _tagsHistory: string[] = [];
   private apiKey: string = environment.api_key;
+  private serviceUrl: string = "https://api.giphy.com/v1/gifs/";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -28,6 +31,17 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.orginizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}search`, { params })
+      .subscribe(resp => {
+        console.log("🚀 ~ file: gifs.service.ts:36 ~ GifsService ~ searchTag ~ resp:", resp)
+        return
+      })
   }
 
 }
