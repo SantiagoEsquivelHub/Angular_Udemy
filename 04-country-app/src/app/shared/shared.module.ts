1d5b44c514087324ed002943b179164b93651a4a
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-11-05 15:30:10 -0500

    Contact page

--- 04-country-app/src/app/shared/shared.module.ts
+++ 04-country-app/src/app/shared/shared.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
     SidebarComponent,
   ],
@@ -19,6 +21,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
     SidebarComponent,
   ]
