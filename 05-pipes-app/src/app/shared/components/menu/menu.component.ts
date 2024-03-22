6e3b7aa626cfa022567b43881213c429ffbf5a6c
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-03-22 18:37:32 -0500

    Change theme and Menu to MenuBar

--- 05-pipes-app/src/app/shared/components/menu/menu.component.ts
+++ 05-pipes-app/src/app/shared/components/menu/menu.component.ts
@@ -12,12 +12,32 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
     this.menuItems = [
       {
-        label: 'New',
-        icon: 'pi pi-fw pi-plus',
+        label: 'Angular Pipes',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Texts and Dates',
+            icon: 'pi pi-align-left'
+          },
+          {
+            label: 'Numbers',
+            icon: 'pi pi-dollar'
+          },
+          {
+            label: 'No commons',
+            icon: 'pi pi-globe'
+          },
+        ]
       },
       {
-        label: 'Delete',
-        icon: 'pi pi-fw pi-trash'
+        label: 'Personalized Pipes',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Other element...',
+            icon: 'pi pi-cog'
+          },
+        ]
       }
     ];
   }
