aae8b8e95498cff39fd8b856b03e49638318b262
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-04-25 12:10:45 -0500

    Complete Heroes CRUD

--- 06-heroes-app/src/app/heroes/services/heroes.service.ts
+++ 06-heroes-app/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -23,5 +23,23 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`)
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw new Error('Hero ID is required to update a hero');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHerobyId(id: string): Observable<boolean> {
+    if (!id) throw new Error('Hero ID is required to delete a hero');
+    return this.http.delete<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(error => of(false)), // Return (Observable) false if the hero is not found
+        map(() => true) // Return (Observable) true if the hero is deleted
+      )
   }
 }
