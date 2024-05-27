ed73fc876f5d54c3bddb70f99bbb8bef4696eb36
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-05-27 08:30:11 -0500

    Reset and set values to my form

--- 07-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-forms-app/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 2500,
+  storage: 6,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
   //   price: new FormControl(0,),
@@ -13,6 +19,10 @@ export class BasicPageComponent {
   // });
 
   constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -24,6 +34,8 @@ export class BasicPageComponent {
     if (this.myForm.invalid) return;
 
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, storage: 0 });
 
   }
 }
