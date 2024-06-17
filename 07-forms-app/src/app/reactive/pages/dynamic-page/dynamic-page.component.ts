da5163305a2b2187cba7f01d6aff3c8c680d3206
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-06-16 20:17:43 -0500

    Dynamic Validations

--- 07-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-forms-app/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -17,8 +17,39 @@ export class DynamicPageComponent {
 
   constructor(private fb: FormBuilder) { }
 
-  get favoritesGames(){
+  get favoritesGames() {
     return this.myForm.controls["favoritesGames"] as FormArray;
+  }
+
+
+
+  getFieldError(field: string): string | null {
+
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+
+      switch (key) {
+        case 'required':
+          return 'This field is required';
+        case 'minlength':
+          return `This field must have at least ${errors[key].requiredLength} characters`;
+        case 'min':
+          return `This field must be ${errors[key].min} or greater`;
+
+      }
+    }
+    return null;
+  }
+
+  isValidField(field: string): boolean | null | ValidationErrors {
+    return this.myForm.controls[field].touched && this.myForm.controls[field].errors;
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].touched && formArray.controls[index].errors;
   }
 
   onSubmit(): void {
